4809f6b51dffff5b290cbbb6d7fe6853b300ceda
Nataliia Tyshchenko <ntysh@softserveinc.com>
2023-10-24 22:15:37 +0300

    Initial commit

---
+++ AI_Lab2/Stats.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_Lab2
+{
+    public static class Stats
+    {
+        private string List<Subjects> _definedSubjects = new List<Subjects>({
+            Subjects.Math,
+            Subjects.Art,
+            Subjects.Chemistry,
+            Subjects.Geometry,
+            Subjects.Algebra,
+            Subjects.PE,
+            Subjects.Biology,
+            Subjects.English,
+            Subjects.Language
+        });
+;
+
+        public static double Salary { get; set; }
+
+        public static bool IsSalaryMatter { get; set; }
+
+        public static List<string> FavouriteSubject { get; set; }
+
+
+    }
+}
---
+++ AI_Lab2/Tree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_Lab2
+{
+    public class TreeNode<T>
+    {
+        private LinkedList<TreeNode<T>> _children;
+
+        public Func<bool, Stats> Rules { get; set; }
+
+        public Func<List<double>> GetModifiers { get; set; }
+
+        public void Traverse(TreeNode<T> treeNode)
+        {
+            foreach (var child in _children)
+            {
+                if(child.Rules())
+            }
+        }
+
+        public LinkedList<TreeNode<T>> GetChildren()
+        {
+            return _children;
+        }
+    }
+}
